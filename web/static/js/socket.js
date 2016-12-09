a3fe2b7464ee664cad5b37f3a52b929e392557d8
Samuel Bach <samuel.z.bach@gmail.com>
2016-12-09 18:23:08 +0100

    tachyons styling: main page

--- web/static/js/socket.js
+++ web/static/js/socket.js
@@ -55,45 +55,42 @@ socket.connect()
 
 // Now that you are connected, you can join channels with a topic:
 let channel           = socket.channel("rooms:" + pageId, {})
-let messagesContainer = $(".requests_messages")
+let messagesContainer = $(".requests")
 
 channel.on("new_msg", payload => {
   console.log("incoming for: " + pageId, payload)
   let date = new Date();
-  let html = `<br/>
-     <div class="panel panel-primary" hidden>
-       <div class="panel-heading">
-        <div class="row">
-          <div class="col-xs-1"><span class="request_method">${payload.method}</span></div>
-          <div class="col-xs-5">on ${payload.url}</div>
-          <div class="col-xs-2">from ${payload.remote_ip}</div>
-          <div class="col-xs-4"><span class="request_time">at ${moment().format('DD.MM.YYYY HH:mm:ss')}</span></div>
-        </div>
-       </div>
+  let html = `
+    <div class="request shadow-7 br2 br--bottom mt4" hidden>
+      <div class="br2 br--top bg-orange h2 pa2 white tc f5">
+        <span class="fl w-10 b">${payload.method}</span>
+        <span class="fl w-40">on ${payload.url}</span>
+        <span class="fl w-20">from ${payload.remote_ip}</span>
+        <span class="fl w-30">at ${moment().format('DD.MM.YYYY HH:mm:ss')}</span>
+      </div>
 
-       <div class="panel-body">
-        <div class="row">
-          <div class="col-sm-6">
-            <ul class="property-list">
-              <li class="request_property">
-                <span class="property_title">Body Params</span>#body_params_placeholder#
-              </li>
-              <li class="request_property">
-                <span class="property_title">Query Params</span>#query_params_placeholder#
-              </li>
-              <li class="request_property">
-                <span class="property_title">Body</span>
-                ${ payload.body ? '<textarea readonly class="body-box form-control" rows="5">' + payload.body + '</textarea>' : '<span class="property_empty">empty<span>' }
-              </li>
-            </ul>
-          </div>
-          <div class="col-sm-6">
-            <ul class="property-list">
-              <li class="request_property">
-                <span class="property_title">Headers</span>#headers_placeholder#
-              </li>
-            </ul>
-          </div>
+       <div class="flex justify-between f6">
+        <div class="w-50">
+          <ul class="list pl3">
+            <li class="mt3">
+              <span class="underline">Body Params</span>#body_params_placeholder#
+            </li>
+            <li class="mt3">
+              <span class="underline">Query Params</span>#query_params_placeholder#
+            </li>
+            <li class="mt3">
+              <span class="underline">Body</span>
+              ${ payload.body ? '<textarea readonly class="mt2 ml2 db pa2 br2 b--light-silver w-90 shadow-inset bg-light-gray" rows="6">' + payload.body + '</textarea>' : constructTableRows(null) }
+            </li>
+          </ul>
+        </div>
+
+        <div class="w-50">
+          <ul class="list pl3">
+            <li class="mt3">
+              <span class="underline">Headers</span>#headers_placeholder#
+            </li>
+          </ul>
         </div>
        </div>
      </div>` 
@@ -103,7 +100,7 @@ channel.on("new_msg", payload => {
   html = html.replace("#body_params_placeholder#", constructTableRows(payload.body_params))
 
   messagesContainer.prepend(html)
-  $(".panel-primary").slideDown("fast", () => {});
+  $(".request").slideDown("fast", () => {});
 })
 
 channel.join()
@@ -111,9 +108,9 @@ channel.join()
   .receive("error", resp => { console.error(`Unable to join: "${pageId}"`, resp) })
 
 function constructTableRows(list) {
-  if (!list || list.length <= 0) return '<span class="property_empty">empty<span>'
-  let rows = '<ul class="inner-list">';
-  list.forEach(x => rows += `<li><b>${x[0]}</b> ${x[1]}</li>`)
+  if (!list || list.length <= 0) return '<span class="silver mt1 ml2 db">empty<span>'
+  let rows = '<ul class="list mt1 pl0 ml2">';
+  list.forEach(x => rows += `<li class="mt1"><b>${x[0]}</b> ${x[1]}</li>`)
   return rows + '</ul>';
 }
 
